71a88c76f1e2e1092342a114e1e0c20d11c71958
sofflen <nixxesgaming@gmail.com>
2024-09-11 15:48:37 +0300

    Initial commit

---
+++ src/test/java/com/study/spring6cloudgateway/Spring6CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring6cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
