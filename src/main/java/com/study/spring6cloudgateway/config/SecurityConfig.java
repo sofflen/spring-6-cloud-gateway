d85d2be56917428ed3d0b2e94f23b7805991ce7c
sofflen <nixxesgaming@gmail.com>
2024-09-18 12:02:08 +0300

    Added Spring Actuator

--- src/main/java/com/study/spring6cloudgateway/config/SecurityConfig.java
+++ src/main/java/com/study/spring6cloudgateway/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.study.spring6cloudgateway.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Order(2)
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
@@ -19,6 +22,17 @@ public class SecurityConfig {
                         authorizeExchangeSpec.anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec ->
                         oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorizeExchangeSpec ->
+                        authorizeExchangeSpec.anyExchange().permitAll());
 
         return http.build();
     }
